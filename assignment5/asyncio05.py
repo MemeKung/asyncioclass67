9cd38a5dc9757f6281ba43ae4a342b095c149568
MemeKung <pada.in2547@gmail.com>
2024-07-10 11:33:28 +0700

    commit

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -4,22 +4,22 @@ from random import random
 async def cook_rice():
     cook_time = 1 + random()
     await asyncio.sleep(cook_time)
-    return f'Rice is ready in {cook_time:.4f} seconds'
+    return f'Rice is ready in {cook_time} seconds'
 
 async def cook_noodle():
     cook_time = 1 + random()
     await asyncio.sleep(cook_time)
-    return f'Noodle is ready in {cook_time:.4f} seconds'
+    return f'Noodle is ready in {cook_time} seconds'
 
 async def cook_curry():
     cook_time = 1 + random()
     await asyncio.sleep(cook_time)
-    return f'Curry is ready in {cook_time:.4f} seconds'
+    return f'Curry is ready in {cook_time} seconds'
 
 async def main():
     tasks = [cook_rice(), cook_noodle(), cook_curry()]
     done, _ = await asyncio.wait([asyncio.create_task(task) for task in tasks], return_when=asyncio.FIRST_COMPLETED)
-    
+
     for task in done:
         print(task.result())
 
